c227c86db1e388af95f0d14456413ff37ce8e75a
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-24 15:06:36 +0300

    created component NewDish; added DishForm;

---
+++ src/containers/NewDish/NewDish.tsx
@@ -0,0 +1,30 @@
+import DishForm from '../../components/DishForm/DishForm';
+import {createDish} from '../../features/dishes/dishesThuks';
+import {useNavigate} from 'react-router-dom';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {selectCreateDishLoading} from '../../features/dishes/dishesSlice';
+import {ApiDish} from '../../types';
+
+const NewDish = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const isCreating = useAppSelector(selectCreateDishLoading);
+
+  const onSubmit = async (dish: ApiDish) => {
+    try {
+      await dispatch(createDish(dish)).unwrap();
+      navigate('/admin');
+    } catch (error) {
+      console.error('Could not create dish!');
+    }
+  };
+
+  return (
+    <div className="container mt-3">
+      <DishForm onSubmit={onSubmit} isLoading={isCreating} />
+      
+    </div>
+  );
+};
+
+export default NewDish;
