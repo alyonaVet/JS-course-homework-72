01b6c4c728929440d16aa8e5a0018012a54f83e9
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-24 15:06:08 +0300

    added createDish;

--- src/features/dishes/dishesSlice.ts
+++ src/features/dishes/dishesSlice.ts
@@ -1,25 +1,42 @@
 import {Dish} from '../../types';
 import {createSlice} from '@reduxjs/toolkit';
+import {createDish} from './dishesThuks';
 
 export interface DishesState {
   items: Dish[];
+  createLoading: boolean;
 }
 
 const initialState: DishesState = {
   items: [],
+  createLoading: false,
 };
 
 export const dishesSlice = createSlice({
   name: 'dishes',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createDish.pending, (state) => {
+        state.createLoading = true;
+      })
+      .addCase(createDish.fulfilled, (state) => {
+        state.createLoading = false;
+      })
+      .addCase(createDish.rejected, (state) => {
+        state.createLoading = false;
+      });
+  },
   selectors: {
     selectDishes: (state) => state.items,
+    selectCreateDishLoading: (state) => state.createLoading,
   }
 });
 
 export const dishesReducer = dishesSlice.reducer;
 
 export const {
-  selectDishes
+  selectDishes,
+  selectCreateDishLoading,
 } = dishesSlice.selectors;
