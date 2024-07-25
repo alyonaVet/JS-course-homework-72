4e9f3091524870b159b04eedaeb644ecbe8eeebf
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-25 20:58:12 +0300

    added fetchOneDish, updateDish, deleteDish;

--- src/features/dishes/dishesThuks.ts
+++ src/features/dishes/dishesThuks.ts
@@ -1,6 +1,6 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {RootState} from '../../app/store';
-import {ApiDish, ApiDishes, Dish} from '../../types';
+import {ApiDish, ApiDishes, Dish, UpdateDishType} from '../../types';
 import axiosApi from '../../axiosApi';
 
 export const createDish = createAsyncThunk<void, ApiDish, { state: RootState }>(
@@ -24,3 +24,31 @@ export const fetchDishes = createAsyncThunk<Dish[], void, { state: RootState }>(
       ...dishes[id],
     }));
   });
+
+export const fetchOneDish = createAsyncThunk<ApiDish, string, { state: RootState }>(
+  'dishes/fetchOne',
+  async (id) => {
+    const { data: dish } = await axiosApi.get<ApiDish | null>(
+      `/dishes/${id}.json`,
+    );
+
+    if (dish === null) {
+      throw new Error('Not found');
+    }
+    return dish;
+  },
+);
+
+export const updateDish = createAsyncThunk<void, UpdateDishType, { state: RootState }>(
+  'dishes/update',
+  async ({ id, dish }) => {
+    await axiosApi.put(`/dishes/${id}.json`, dish);
+  },
+);
+
+export const deleteDish = createAsyncThunk<void, string, { state: RootState }>(
+  'dishes/deleteDish',
+  async (dishId) => {
+    await axiosApi.delete('/dishes/' + dishId + '.json');
+  },
+);
