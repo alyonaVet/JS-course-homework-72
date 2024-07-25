c2ee065a39b99583b37612c6b323955648596c09
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-25 20:56:14 +0300

    refactor styles DishItem;

--- src/components/Dish/DishItem.tsx
+++ src/components/Dish/DishItem.tsx
@@ -1,42 +1,23 @@
 import React from 'react';
 import {Dish} from '../../types';
-import {NavLink} from 'react-router-dom';
-import ButtonSpinner from '../Spinner/ButtonSpinner';
 
 interface Props {
   dish: Dish;
-  onDelete: VoidFunction;
-  deleteLoading: false | string;
+  children?: React.ReactNode;
 }
 
-const DishItem: React.FC<Props> = ({dish, onDelete, deleteLoading}) => {
+const DishItem: React.FC<Props> = ({dish, children}) => {
 
   return (
-    <div className="card mb-2 p-2">
-      <div className="row g-0 align-items-center">
-        <div className="col-8 d-flex align-items-center">
+    <div className="card mb-2 px-3 py-2">
+      <div className="d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center">
           <img src={dish.image} alt={dish.title} className="rounded float-start"
                style={{width: '100px', height: '100px'}}/>
           <span className="card-title ms-4 fs-4">{dish.title}</span>
         </div>
-        <div className="col-2">
-          <div className="card-text fw-semibold">{dish.price} KGS</div>
-        </div>
-        <div className="col-2">
-          <div className="d-flex gap-2">
-            <NavLink className="btn btn-primary px-4" to={`/admin/edit-dish/${dish.id}`}>
-              Edit
-            </NavLink>
-            <button
-              className="btn btn-danger"
-              onClick={onDelete}
-              disabled={deleteLoading ? deleteLoading === dish.id : false}
-            >
-              {deleteLoading && deleteLoading === dish.id && (<ButtonSpinner/>)}
-              Delete
-            </button>
-          </div>
-        </div>
+        <div className="card-text fw-semibold me-4">{dish.price} KGS</div>
+        {children}
       </div>
     </div>
   );
