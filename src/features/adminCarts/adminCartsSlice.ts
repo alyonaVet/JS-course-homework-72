81e632e6fbad2f83cf530874461df4fcfa115ba9
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 16:22:39 +0300

    added deleteLoading;

--- src/features/adminCarts/adminCartsSlice.ts
+++ src/features/adminCarts/adminCartsSlice.ts
@@ -1,15 +1,18 @@
 import {ApiOrders} from '../../types';
 import {createSlice} from '@reduxjs/toolkit';
-import {fetchOrders} from './adminCartsThunk';
+import {deleteOrder, fetchOrders} from './adminCartsThunk';
+import {deleteDish} from '../dishes/dishesThuks';
 
 export interface AdminCartsState {
   adminCarts: ApiOrders;
   fetchLoading: boolean;
+  deleteLoading: false | string;
 }
 
 const initialState: AdminCartsState = {
   adminCarts: {},
   fetchLoading: false,
+  deleteLoading: false,
 }
 
 export const adminCartsSlice = createSlice({
@@ -28,10 +31,22 @@ export const adminCartsSlice = createSlice({
       .addCase(fetchOrders.rejected, (state) => {
         state.fetchLoading = false;
       });
+    builder
+      .addCase(deleteDish.pending, (state, { meta: { arg: orderId } }) => {
+        state.deleteLoading = orderId;
+      })
+      .addCase(deleteOrder.fulfilled, (state) => {
+        state.deleteLoading = false;
+      })
+      .addCase(deleteOrder.rejected, (state) => {
+        state.deleteLoading = false;
+      });
   },
   selectors: {
     selectAdminCarts: (state) => state.adminCarts,
     selectFetchAdminCartsLoading: (state) => state.fetchLoading,
+    selectDeleteOrderLoading: (state) => state.deleteLoading,
+
   }
 });
 
@@ -40,4 +55,5 @@ export const adminCartsReducer = adminCartsSlice.reducer;
 export const {
   selectAdminCarts,
   selectFetchAdminCartsLoading,
+  selectDeleteOrderLoading,
 } = adminCartsSlice.selectors;
