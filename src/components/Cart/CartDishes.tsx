c64e6d0fe5405095be39f71fc2375f6a1e03ed98
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 12:13:07 +0300

    created component CartDishes;

---
+++ src/components/Cart/CartDishes.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import CartItem from './CartItem';
+import { CartDish } from '../../types';
+import {useAppDispatch} from '../../app/hooks';
+import {deleteDish} from '../../features/cart/cartSlice';
+
+interface Props {
+  cartDishes: CartDish[];
+}
+
+let delivery = 150;
+
+const CartDishes: React.FC<Props> = ({ cartDishes }) => {
+  const dispatch = useAppDispatch();
+
+  const total = cartDishes.reduce((sum, cartDish) => {
+    return sum + (cartDish.amount * cartDish.dish.price);
+  }, 0);
+
+
+  return (
+    <>
+      {cartDishes.map((cartDish) => (
+        <CartItem key={cartDish.dish.id} cartDish={cartDish} onDelete={() => dispatch(deleteDish(cartDish))} />
+      ))}
+      <div className="card border-0 p-2">
+        <div className="row">
+          <div className="col"><strong>Delivery:</strong></div>
+          <div className="col-3 text-end text-nowrap">
+            {delivery} KGS
+          </div>
+        </div>
+      </div>
+      <div className="card border-0 p-2">
+        <div className="row">
+          <div className="col"><strong>Total:</strong></div>
+          <div className="col-3 text-end text-nowrap">
+            <strong>{total + delivery}</strong> KGS
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+export default CartDishes;
