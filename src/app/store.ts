d5b339dfbbc9c18e00010e339c4a03f52c8bae0f
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 13:13:03 +0300

    added state adminCarts;

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {dishesReducer} from '../features/dishes/dishesSlice';
 import {cartReducer} from '../features/cart/cartSlice';
+import {adminCartsReducer} from '../features/adminCarts/adminCartsSlice';
 
 export const store = configureStore({
   reducer: {
     dishes: dishesReducer,
     cart: cartReducer,
+    adminCarts: adminCartsReducer,
   },
 });
 
