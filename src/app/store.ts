58b333c719d61c3d79789ea078e40dc1da1a37c8
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 12:14:13 +0300

    added state cart;

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {dishesReducer} from '../features/dishes/dishesSlice';
+import {cartReducer} from '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
     dishes: dishesReducer,
+    cart: cartReducer,
   },
 });
 
