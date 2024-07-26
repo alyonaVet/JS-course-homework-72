42767c76b0e8630d498b9eb5ffafd45267321940
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 12:15:51 +0300

    refactor ClientPage; added  totalAmount;

--- src/containers/ClientPage/ClientPage.tsx
+++ src/containers/ClientPage/ClientPage.tsx
@@ -1,41 +1,58 @@
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
 import {selectDishes, selectFetchDishesLoading} from '../../features/dishes/dishesSlice';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {fetchDishes} from '../../features/dishes/dishesThuks';
 import Spinner from '../../components/Spinner/Spinner';
 import DishItem from '../../components/Dish/DishItem';
+import Modal from '../../components/Modal/Modal';
+import Cart from '../../components/Cart/Cart';
+import {addDish, selectCartDishes} from '../../features/cart/cartSlice';
 
 const ClientPage = () => {
+  const [showModal, setShowModal] = useState(false);
   const dishes = useAppSelector(selectDishes);
   const dispatch = useAppDispatch();
   const dishesLoading = useAppSelector(selectFetchDishesLoading);
-
+  const cartDishes = useAppSelector(selectCartDishes);
 
   useEffect(() => {
     dispatch(fetchDishes());
   }, [dispatch]);
 
+  const totalAmount = cartDishes.reduce((acc, cartDish) => {
+    return acc + cartDish.dish.price * cartDish.amount;
+  }, 0);
+
   return (
-    <div className="container mt-4">
-      <div>
-        {dishesLoading ? (
-          <Spinner/>
-        ) : (
-          dishes.map((dish) => (
-            <DishItem
-              key={dish.id}
-              dish={dish}
-            />
-          ))
-        )}
+    <>
+      <div className="container mt-4">
         <div>
-          <div>Order total:</div>
+          {dishesLoading ? (
+            <Spinner/>
+          ) : (
+            dishes.map((dish) => (
+              <DishItem
+                key={dish.id}
+                dish={dish}
+                onAdd={() => dispatch(addDish(dish))}
+              />
+            ))
+          )}
+
+        </div>
+        <div className="mt-3 pt-3 d-flex justify-content-between align-items-center border-top">
+          <div>Order total: {totalAmount}</div>
+          {cartDishes.map((cartDish, key) => (
+            <p key={key}>{cartDish.dish.title} : {cartDish.amount}</p>
+          ))}
+          <button type="button" className="btn btn-primary" onClick={() => setShowModal(true)}>Checkout</button>
         </div>
       </div>
-      <div>
+      <Modal show={showModal} title="Your order:" onClose={() => setShowModal(false)}>
+        <Cart onClose={() => setShowModal(false)}/>
+      </Modal>
 
-      </div>
-    </div>
+    </>
   );
 };
 
