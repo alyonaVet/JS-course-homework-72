b6fb6c4b4d96d47ccd483b3ecbfe9a2e732ad042
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 12:14:42 +0300

    refactor DishItem; added addItem;

--- src/components/Dish/DishItem.tsx
+++ src/components/Dish/DishItem.tsx
@@ -3,13 +3,14 @@ import {Dish} from '../../types';
 
 interface Props {
   dish: Dish;
+  onAdd: VoidFunction;
   children?: React.ReactNode;
 }
 
-const DishItem: React.FC<Props> = ({dish, children}) => {
+const DishItem: React.FC<Props> = ({dish, onAdd, children }) => {
 
   return (
-    <div className="card mb-2 px-3 py-2">
+    <div className="card mb-2 px-3 py-2" onClick={onAdd} role="button">
       <div className="d-flex align-items-center justify-content-between">
         <div className="d-flex align-items-center">
           <img src={dish.image} alt={dish.title} className="rounded float-start"
