9cde32fcc65b07a00b3481eba1af2988befa11b2
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-26 16:24:52 +0300

    added Routes; added loading dishes when App load;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-const App = () => {
+import Toolbar from './components/Toolbar/Toolbar';
+import AdminPage from './containers/AdminPage/AdminPage';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import NewDish from './containers/NewDish/NewDish';
+import EditDish from './containers/EditDish/EditDish';
+import ClientPage from './containers/ClientPage/ClientPage';
+import Orders from './containers/Orders/Orders';
+import {useEffect} from 'react';
+import {fetchDishes} from './features/dishes/dishesThuks';
+import {useAppDispatch} from './app/hooks';
 
-    return (
-        <>
-            Hello World!
-        </>
-    );
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDishes());
+  }, [dispatch]);
+
+  return (
+    <>
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<ClientPage />} />
+          <Route path="/admin" element={<Navigate to="/admin/dishes" replace={true}/>}/>
+          <Route path="/admin/dishes" element={<AdminPage/>}/>
+          <Route path="/admin/new-dish" element={<NewDish/>}/>
+          <Route path="/admin/edit-dish/:id" element={<EditDish/>}/>
+          <Route path="/admin/order" element={<Orders />} />
+          <Route path="*" element={<h3 className="mt-3 mb-5 text-center">Page not found</h3>}/>
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
